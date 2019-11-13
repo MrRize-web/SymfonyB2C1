78852644de975ef7d7f1dd0e0b08e84f794e255c
MrRize-web <54984438+MrRize-web@users.noreply.github.com>
2019-11-13 23:25:59 +0100

    Symfony B1C1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+
+    }
+    /**
+ * @Route("/DireBonjour/{prenom}", name="test")
+ */
+    public function test($prenom)
+    {
+        return $this->render('home/index.html.twig', [
+            'prenom'=>"$prenom",
         ]);
     }
 }
